da19cef5bfa0d1edabab94b61db8eaa871ab56d9
João Paulo <paulocorrentinense@gmail.com>
2023-08-28 09:37:55 -0300

    v2

--- script.js
+++ script.js
@@ -1,2 +1,52 @@
 const divDeResultado = document.getElementById('telaDeResultado')
-const todosOsTds = document.querySelectorAll('.numeroClicado')
+const container = document.getElementById('container')
+// console.log(container)
+const teclas = container.querySelector('#tabela')
+// console.log(teclas)
+teclas.addEventListener('click', e => {    //Evento que ouve todas as teclas 
+    if(e.target.matches('td')) {             //O matches retorna verdadeiro ou falso, ouseja, clicado ou não    
+        const key = e.target                   // A variável key recebe a tecla que foi clicada(alvo)
+        const action = key.dataset.action     //A variável action recebe as teclas que tem o atributo action
+        if(!action) {                        //verifica se o atributo data da tecla clicada é igual action 
+            divDeResultado.innerText += key.textContent
+        }
+        if(
+            action === 'adicao' ||
+            action === 'subtracao' ||
+            action === 'multipicacao' ||  //verifica se a tecla clicada é um operador
+            action === 'divisao' ||
+            action === 'porcentagem'
+        ) {
+    
+        }
+        if (action === 'decimal') {
+            
+          }
+          
+          if (action === 'limpar') {
+            
+          }
+
+          if (action === 'limparTudo') {
+            divDeResultado.innerText = '0'
+          }
+          
+          if (action === 'calculate') {
+            
+          }
+    }
+})
+
+
+
+// const minhaTabela =document.getElementById('tabela')
+// console.log(minhaTabela)
+
+// const linha = minhaTabela.rows[1]
+// console.log(linha)
+
+// const celula = linha.cells[0]
+// console.log(celula.textContent)
+
+// const igual = document.getElementById('igual')
+// console.log(igual.textContent)
